637deab5b04d688f580bfb1326fa2ccf721a071c
Gaurav Singh <grvsgh7@gmail.com>
2023-09-04 20:28:11 +0530

    first commit

---
+++ src/main/java/com/sfg/recipe/app/SfgRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.recipe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfg/recipe/app/SfgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.recipe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
