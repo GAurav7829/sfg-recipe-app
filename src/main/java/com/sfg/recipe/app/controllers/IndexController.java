ea8e76f179d36c1c2685a154b525a112cd37de4d
Gaurav Singh <grvsgh7@gmail.com>
2023-09-07 20:22:10 +0530

    JPA query methods example

--- src/main/java/com/sfg/recipe/app/controllers/IndexController.java
+++ src/main/java/com/sfg/recipe/app/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package com.sfg.recipe.app.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sfg.recipe.app.model.Category;
+import com.sfg.recipe.app.model.UnitOfMeasure;
+import com.sfg.recipe.app.repositories.CategoryRepository;
+import com.sfg.recipe.app.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({ "", "/", "/index" })
 	public String getIndexPage() {
+		Optional<Category> american = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> cup = unitOfMeasureRepository.findByDescription("Cup");
+
+		System.out.println(american.get().getId() + " : " + american.get().getDescription());
+		System.out.println(cup.get().getId() + " : " + cup.get().getDescription());
+
 		return "index";
 	}
 
