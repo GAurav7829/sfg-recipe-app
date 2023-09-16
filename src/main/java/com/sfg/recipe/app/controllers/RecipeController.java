bcee29ace92298be0f489adf26ba5f28b22ae0b1
Gaurav Singh <grvsgh7@gmail.com>
2023-09-16 20:04:46 +0530

    added bad request exception

--- src/main/java/com/sfg/recipe/app/controllers/RecipeController.java
+++ src/main/java/com/sfg/recipe/app/controllers/RecipeController.java
@@ -1,15 +1,25 @@
 package com.sfg.recipe.app.controllers;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sfg.recipe.app.commands.RecipeCommand;
+import com.sfg.recipe.app.exceptions.NotFoundException;
 import com.sfg.recipe.app.services.RecipeService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -48,6 +58,28 @@ public class RecipeController {
 	public String deleteRecipe(@PathVariable String id) {
 		recipeService.deleleById(Long.valueOf(id));
 		return "redirect:/";
+	}
+	
+	@ResponseStatus(NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound(Exception exception) {
+		log.debug("handling not found exception");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("error");
+		modelAndView.addObject("exceptionType", "404 Not Found!!!");
+		modelAndView.addObject("exception", exception.getMessage());
+		return modelAndView;
+	}
+	
+	@ResponseStatus(BAD_REQUEST)
+	@ExceptionHandler(NumberFormatException.class)
+	public ModelAndView handleNumberFormat(Exception exception) {
+		log.debug("handling number format exception");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("error");
+		modelAndView.addObject("exceptionType", "400 Bad Request!!!");
+		modelAndView.addObject("exception", exception.getMessage());
+		return modelAndView;
 	}
 
 }
--- src/main/java/com/sfg/recipe/app/services/RecipeServiceImpl.java
+++ src/main/java/com/sfg/recipe/app/services/RecipeServiceImpl.java
@@ -44,7 +44,7 @@ public class RecipeServiceImpl implements RecipeService {
 	public Recipe findById(Long id) {
 		Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
 		if (!optionalRecipe.isPresent())
-			throw new NotFoundException("Recipe not found");
+			throw new NotFoundException("Recipe not found. For id: "+id);
 		return optionalRecipe.get();
 	}
 
